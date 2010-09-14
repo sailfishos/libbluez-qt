df676c0998b7ddb69453f82d3f78fbf90b771400
Kevron Rees <kevron_m_rees@linux.intel.com>
2010-09-14 11:38:27 -0700

    added license file

--- bluetoothdevicemodel.cpp
+++ bluetoothdevicemodel.cpp
@@ -1,3 +1,14 @@
+/*  -*- Mode: C++ -*-
+ *
+ * meego handset bluetooth
+ * Copyright © 2010, Intel Corporation.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Apache License, version 2.0.  The full text of the Apache License is at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
 #include "bluetoothdevicemodel.h"
 
 BluetoothDevicesModel::BluetoothDevicesModel(QObject *parent) :
