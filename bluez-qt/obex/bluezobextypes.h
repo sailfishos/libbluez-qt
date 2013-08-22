f093ab9115060caaf8cf4c5985731076b22d9c3b
Bea Lam <bea.lam@jollamobile.com>
2013-08-22 14:54:08 +1000

    [libbluez-qt] Complete port to obexd-0.48
    Fixes to reflect the API changes in obexd-0.48:
    
    - OPP functionality has moved from ObexClientInterface to a new
      ObexObjectPushInterface class.
    - ObexFtp class has been removed; the ObexFileTransfer class, which is
      a direct interface to the dbus API, can be used instead.
    - Transfer notifications (progress, complete, etc.) have moved from the
      ObexAgentAdaptor to the ObexTransferInterface
    - Various function arguments and return values have changed.

--- bluez-qt/obex/bluezobextypes.h
+++ bluez-qt/obex/bluezobextypes.h
@@ -4,8 +4,13 @@
 #include <QtCore/QMap>
 #include <QtCore/QVariantMap>
 #include <QtCore/QString>
+#include <QtDBus/QDBusObjectPath>
 
 typedef QList<QVariantMap> QVariantMapList;
 Q_DECLARE_METATYPE(QVariantMapList)
+
+// note this must be registered using qDBusRegisterMetaType() before use
+typedef QPair<QDBusObjectPath, QVariantMap> QObexObjectData;
+Q_DECLARE_METATYPE(QObexObjectData)
 
 #endif
