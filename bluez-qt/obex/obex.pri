sailfishos/libbluez-qt

commit f093ab9115060caaf8cf4c5985731076b22d9c3b

[libbluez-qt] Complete port to obexd-0.48
Fixes to reflect the API changes in obexd-0.48:

- OPP functionality has moved from ObexClientInterface to a new
  ObexObjectPushInterface class.
- ObexFtp class has been removed; the ObexFileTransfer class, which is
  a direct interface to the dbus API, can be used instead.
- Transfer notifications (progress, complete, etc.) have moved from the
  ObexAgentAdaptor to the ObexTransferInterface
- Various function arguments and return values have changed.

bluez-qt/obex/obex.pri
@@ -1,14 +1,22 @@
 !exists(obexagent.h):system(qdbusxml2cpp -c ObexAgentAdaptor -a obexagent.h:obexagent.cpp agent.xml)
-!exists(obexclient.h):system(qdbusxml2cpp -p obexclient.h:obexclient.cpp client.xml)
-!exists(obexsession.h):system(qdbusxml2cpp -p obexsession.h:obexsession.cpp -i types.h session.xml)
-!exists(obextransfer.h):system(qdbusxml2cpp -p obextransfer.h:obextransfer.cpp transfer.xml)
+!exists(obexclient.h):system(qdbusxml2cpp -c ObexClientInterface -p obexclient.h:obexclient.cpp client.xml)
+!exists(obexclient.h):system(qdbusxml2cpp -c ObexObjectPushInterface -p obexobjectpush.h:obexobjectpush.cpp -i bluezobextypes.h objectpush.xml)
+!exists(obexfiletransfer.h):system(qdbusxml2cpp -c ObexFileTransferInterface -p obexfiletransfer.h:obexfiletransfer.cpp -i bluezobextypes.h filetransfer.xml)
+!exists(obexsession.h):system(qdbusxml2cpp -c ObexSessionInterface -p obexsession.h:obexsession.cpp session.xml)
+!exists(obextransfer.h):system(qdbusxml2cpp -c ObexTransferInterface -p obextransfer.h:obextransfer.cpp transfer.xml)
+
 SOURCES += obex/obexagent.cpp \
     obex/obexclient.cpp \
+    obex/obexobjectpush.cpp \
+    obex/obexfiletransfer.cpp \
     obex/obexsession.cpp \
-    obex/obextransfer.cpp \
-    obex/obexftp.cpp
+    obex/obextransfer.cpp
+
 HEADERS += obex/obexagent.h \
     obex/obexclient.h \
+    obex/obexobjectpush.h \
+    obex/obexfiletransfer.h \
     obex/obexsession.h \
     obex/obextransfer.h \
-    obex/obexftp.h
+    obex/bluezobextypes.h
+

bluez-qt/obex/agent.xml
@@ -2,18 +2,15 @@
           "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
 <node>
   <interface name="org.bluez.obex.Agent">
-    <method name="Release">
+    <method name="Authorize">
+      <arg type="o" direction="in" name="transfer"/>
+      <arg type="s" direction="in" name="bt_address"/>
+      <arg type="s" direction="in" name="name"/>
+      <arg type="s" direction="in" name="type"/>
+      <arg type="i" direction="in" name="length"/>
+      <arg type="i" direction="in" name="time"/>
+      <arg type="s" direction="out"/>
     </method>
-    <method name="Progress">
-      <arg direction="in"  type="o" name="path" />
-      <arg direction="in"  type="t" name="transferred" />
-    </method>
-    <method name="Complete">
-      <arg direction="in"  type="o" name="path" />
-    </method>
-    <method name="Error">
-      <arg direction="in"  type="o" name="path" />
-      <arg direction="in"  type="s" name="error" />
-    </method>
+    <method name="Cancel"/>
   </interface>
 </node>

bluez-qt/obex/bluezobextypes.h
@@ -4,8 +4,13 @@
 #include <QtCore/QMap>
 #include <QtCore/QVariantMap>
 #include <QtCore/QString>
+#include <QtDBus/QDBusObjectPath>
 
 typedef QList<QVariantMap> QVariantMapList;
 Q_DECLARE_METATYPE(QVariantMapList)
+
+// note this must be registered using qDBusRegisterMetaType() before use
+typedef QPair<QDBusObjectPath, QVariantMap> QObexObjectData;
+Q_DECLARE_METATYPE(QObexObjectData)
 
 #endif

bluez-qt/obex/client.xml
@@ -2,32 +2,14 @@
           "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
 <node>
   <interface name="org.bluez.obex.Client">
-    <method name="SendFile">
-      <arg type="a{sv}" direction="in"/>
-      <arg type="s" direction="in"/>
-      <arg type="o" direction="in"/>
-      <annotation name="org.qtproject.QtDBus.QtTypeName.In0" value="QVariantMap"/>
-    </method>
-    <method name="PullBusinessCard">
-      <arg type="a{sv}" direction="in"/>
-      <arg type="s" direction="in"/>
-      <annotation name="org.qtproject.QtDBus.QtTypeName.In0" value="QVariantMap"/>
-    </method>
-    <method name="ExchangeBusinessCards">
-      <arg type="a{sv}" direction="in"/>
-      <arg type="s" direction="in"/>
-      <arg type="s" direction="in"/>
-      <annotation name="org.qtproject.QtDBus.QtTypeName.In0" value="QVariantMap"/>
-    </method>
     <method name="CreateSession">
+      <arg type="s" direction="in"/>
       <arg type="a{sv}" direction="in"/>
       <arg type="o" direction="out"/>
-      <annotation name="org.qtproject.QtDBus.QtTypeName.In0" value="QVariantMap"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName.In1" value="QVariantMap"/>
     </method>
-    <method name="GetCapabilities">
-      <arg type="a{sv}" direction="in"/>
-      <arg type="s" direction="out"/>
-      <annotation name="org.qtproject.QtDBus.QtTypeName.In0" value="QVariantMap"/>
+    <method name="RemoveSession">
+      <arg type="o" direction="in"/>
     </method>
   </interface>
 </node>

bluez-qt/obex/filetransfer.xml
@@ -1,0 +1,39 @@
+<!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+          "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+<node>
+  <interface name="org.bluez.obex.FileTransfer">
+    <method name="ChangeFolder">
+      <arg type="s" direction="in"/>
+    </method>
+    <method name="CreateFolder">
+      <arg type="s" direction="in"/>
+    </method>
+    <method name="ListFolder">
+      <arg type="aa{sv}" direction="out"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName.Out0" value="QVariantMapList"/>
+    </method>
+    <method name="GetFile">
+      <arg type="s" direction="in"/>
+      <arg type="s" direction="in"/>
+      <arg type="(oa{sv})" direction="out"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName.Out0" value="QObexObjectData"/>
+    </method>
+    <method name="PutFile">
+      <arg type="s" direction="in"/>
+      <arg type="s" direction="in"/>
+      <arg type="(oa{sv})" direction="out"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName.Out0" value="QObexObjectData"/>
+    </method>
+    <method name="CopyFile">
+      <arg type="s" direction="in"/>
+      <arg type="s" direction="in"/>
+    </method>
+    <method name="MoveFile">
+      <arg type="s" direction="in"/>
+      <arg type="s" direction="in"/>
+    </method>
+    <method name="Delete">
+      <arg type="s" direction="in"/>
+    </method>
+  </interface>
+</node>

bluez-qt/obex/obexagent.cpp
@@ -1,8 +1,8 @@
 /*
- * This file was generated by qdbusxml2cpp version 0.7
+ * This file was generated by qdbusxml2cpp version 0.8
  * Command line was: qdbusxml2cpp -c ObexAgentAdaptor -a obexagent.h:obexagent.cpp agent.xml
  *
- * qdbusxml2cpp is Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+ * qdbusxml2cpp is Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
  *
  * This is an auto-generated file.
  * Do not edit! All changes made to it will be lost.
@@ -33,27 +33,17 @@ ObexAgentAdaptor::~ObexAgentAdaptor()
     // destructor
 }
 
-void ObexAgentAdaptor::Complete(const QDBusObjectPath &path)
+QString ObexAgentAdaptor::Authorize(const QDBusObjectPath &transfer, const QString &bt_address, const QString &name, const QString &type, int length, int time)
 {
-    // handle method call org.bluez.obex.Agent.Complete
-    QMetaObject::invokeMethod(parent(), "Complete", Q_ARG(QDBusObjectPath, path));
+    // handle method call org.bluez.obex.Agent.Authorize
+    QString out0;
+    QMetaObject::invokeMethod(parent(), "Authorize", Q_RETURN_ARG(QString, out0), Q_ARG(QDBusObjectPath, transfer), Q_ARG(QString, bt_address), Q_ARG(QString, name), Q_ARG(QString, type), Q_ARG(int, length), Q_ARG(int, time));
+    return out0;
 }
 
-void ObexAgentAdaptor::Error(const QDBusObjectPath &path, const QString &error)
+void ObexAgentAdaptor::Cancel()
 {
-    // handle method call org.bluez.obex.Agent.Error
-    QMetaObject::invokeMethod(parent(), "Error", Q_ARG(QDBusObjectPath, path), Q_ARG(QString, error));
-}
-
-void ObexAgentAdaptor::Progress(const QDBusObjectPath &path, qulonglong transferred)
-{
-    // handle method call org.bluez.obex.Agent.Progress
-    QMetaObject::invokeMethod(parent(), "Progress", Q_ARG(QDBusObjectPath, path), Q_ARG(qulonglong, transferred));
-}
-
-void ObexAgentAdaptor::Release()
-{
-    // handle method call org.bluez.obex.Agent.Release
-    QMetaObject::invokeMethod(parent(), "Release");
+    // handle method call org.bluez.obex.Agent.Cancel
+    QMetaObject::invokeMethod(parent(), "Cancel");
 }
 

bluez-qt/obex/obexagent.h
@@ -1,25 +1,27 @@
 /*
- * This file was generated by qdbusxml2cpp version 0.7
+ * This file was generated by qdbusxml2cpp version 0.8
  * Command line was: qdbusxml2cpp -c ObexAgentAdaptor -a obexagent.h:obexagent.cpp agent.xml
  *
- * qdbusxml2cpp is Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+ * qdbusxml2cpp is Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
  *
  * This is an auto-generated file.
  * This file may have been hand-edited. Look for HAND-EDIT comments
  * before re-generating it.
  */
 
-#ifndef OBEXAGENT_H_QDBUSXML2CPP
-#define OBEXAGENT_H_QDBUSXML2CPP
+#ifndef OBEXAGENT_H_1377151098
+#define OBEXAGENT_H_1377151098
 
 #include <QtCore/QObject>
 #include <QtDBus/QtDBus>
+QT_BEGIN_NAMESPACE
 class QByteArray;
 template<class T> class QList;
 template<class Key, class Value> class QMap;
 class QString;
 class QStringList;
 class QVariant;
+QT_END_NAMESPACE
 
 /*
  * Adaptor class for interface org.bluez.obex.Agent
@@ -30,18 +32,16 @@ class ObexAgentAdaptor: public QDBusAbstractAdaptor
     Q_CLASSINFO("D-Bus Interface", "org.bluez.obex.Agent")
     Q_CLASSINFO("D-Bus Introspection", ""
 "  <interface name=\"org.bluez.obex.Agent\">\n"
-"    <method name=\"Release\"/>\n"
-"    <method name=\"Progress\">\n"
-"      <arg direction=\"in\" type=\"o\" name=\"path\"/>\n"
-"      <arg direction=\"in\" type=\"t\" name=\"transferred\"/>\n"
+"    <method name=\"Authorize\">\n"
+"      <arg direction=\"in\" type=\"o\" name=\"transfer\"/>\n"
+"      <arg direction=\"in\" type=\"s\" name=\"bt_address\"/>\n"
+"      <arg direction=\"in\" type=\"s\" name=\"name\"/>\n"
+"      <arg direction=\"in\" type=\"s\" name=\"type\"/>\n"
+"      <arg direction=\"in\" type=\"i\" name=\"length\"/>\n"
+"      <arg direction=\"in\" type=\"i\" name=\"time\"/>\n"
+"      <arg direction=\"out\" type=\"s\"/>\n"
 "    </method>\n"
-"    <method name=\"Complete\">\n"
-"      <arg direction=\"in\" type=\"o\" name=\"path\"/>\n"
-"    </method>\n"
-"    <method name=\"Error\">\n"
-"      <arg direction=\"in\" type=\"o\" name=\"path\"/>\n"
-"      <arg direction=\"in\" type=\"s\" name=\"error\"/>\n"
-"    </method>\n"
+"    <method name=\"Cancel\"/>\n"
 "  </interface>\n"
         "")
 public:
@@ -50,10 +50,8 @@ public:
 
 public: // PROPERTIES
 public Q_SLOTS: // METHODS
-    void Complete(const QDBusObjectPath &path);
-    void Error(const QDBusObjectPath &path, const QString &error);
-    void Progress(const QDBusObjectPath &path, qulonglong transferred);
-    void Release();
+    QString Authorize(const QDBusObjectPath &transfer, const QString &bt_address, const QString &name, const QString &type, int length, int time);
+    void Cancel();
 Q_SIGNALS: // SIGNALS
 };
 

bluez-qt/obex/obexclient.cpp
@@ -1,8 +1,8 @@
 /*
- * This file was generated by qdbusxml2cpp version 0.7
+ * This file was generated by qdbusxml2cpp version 0.8
  * Command line was: qdbusxml2cpp -c ObexClientInterface -p obexclient.h:obexclient.cpp client.xml
  *
- * qdbusxml2cpp is Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
+ * qdbusxml2cpp is Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
  *
  * This is an auto-generated file.
  * This file may have been hand-edited. Look for HAND-EDIT comments

commit af1c507088532397ca477ec2f3291973b4885223

Fix typo in project file (executing qdbusxml2cpp)
Introduced by f093ab9

Relates NEMO#699

bluez-qt/obex/obex.pri
@@ -1,6 +1,6 @@
 !exists(obexagent.h):system(qdbusxml2cpp -c ObexAgentAdaptor -a obexagent.h:obexagent.cpp agent.xml)
 !exists(obexclient.h):system(qdbusxml2cpp -c ObexClientInterface -p obexclient.h:obexclient.cpp client.xml)
-!exists(obexclient.h):system(qdbusxml2cpp -c ObexObjectPushInterface -p obexobjectpush.h:obexobjectpush.cpp -i bluezobextypes.h objectpush.xml)
+!exists(obexobjectpush.h):system(qdbusxml2cpp -c ObexObjectPushInterface -p obexobjectpush.h:obexobjectpush.cpp -i bluezobextypes.h objectpush.xml)
 !exists(obexfiletransfer.h):system(qdbusxml2cpp -c ObexFileTransferInterface -p obexfiletransfer.h:obexfiletransfer.cpp -i bluezobextypes.h filetransfer.xml)
 !exists(obexsession.h):system(qdbusxml2cpp -c ObexSessionInterface -p obexsession.h:obexsession.cpp session.xml)
 !exists(obextransfer.h):system(qdbusxml2cpp -c ObexTransferInterface -p obextransfer.h:obextransfer.cpp transfer.xml)